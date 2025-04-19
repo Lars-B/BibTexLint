02f1394d7249d634db02d5f79ba4e2243d600322
Lars-B <berlinglars96@gmail.com>
2025-04-18 22:41:33 -0700

    WIP: api call an citation for biorxiv mostly done, need to find strategies for arxiv publicaitons next

--- src/bibtexlint/_preprint_strategies.py
+++ src/bibtexlint/_preprint_strategies.py
@@ -1,57 +1,86 @@
-from abc import ABC, abstractmethod
+import requests
 
 
-class PreprintStrategy(ABC):
+class PreprintHandler():
+
+    def __init__(self, name: str, config: dict):
+        self.name = name
+        self.api_url = config.get("api_url")
+        self.published_key = config.get("published_key")
 
     def _make_api_request(self, doi):
         """Common logic to make an API request."""
-        # Perform the API call that is shared between journals
-        print("HELLO, WE HAVE TO MAKE THE API REQUEST AND ALL THAT HERE")
-        # response = requests.get(f"https://api.somepreprintserver.org/content/{doi}")
-        # if response.status_code == 200:
-        #     return response.json()
-        # else:
-        #     print(f"Error fetching preprint with DOI: {doi}")
-        #     return None
+        # Perform the API call
+        response = requests.get(f"{self.api_url}/{doi}")
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise ValueError(f"API request failed with status code "
+                             f"{response.status_code}...")
 
-    @abstractmethod
+    def _find_doi(self):
+        """
+        This function will try to find the DOI for a preprint.
+        :return:
+        :rtype:
+        """
+        doi = ""
+        # todo try to find doi for an entry that does not have one...
+        return doi
+
+    def handle_published(self, data):
+        published_entry = ""
+        # todo need to do doi2bib with the published doi.
+        published_doi = data['collection'][0][self.published_key]
+        # test = f"https://doi.org/{published_doi}"
+        test = {
+            "url": f"https://doi.org/{published_doi}",
+            "headers": {
+                "Accept": "application/x-bibtex; charset=utf-8"
+            }
+        }
+        response = requests.get(test["url"], headers=test["headers"])
+        if response.status_code != 200:
+            raise ValueError(f"API request failed with status code "
+                             f"{response.status_code}...")
+        new_citation = response.text
+        return published_entry
+
     def update_preprint(self, entry):
-        pass
-
-
-# strategy_pattern.py
-class BioRxivPreprintStrategy(PreprintStrategy):
-    def update_preprint(self, entry):
-        print(f"Checking bioRxiv preprint: {entry['fields']['title']}")
-        return entry
-        # result = self._make_api_request(entry['fields']['doi'])
-        # if result:
-        #     print(f"bioRxiv entry found: {result}")
-        # else:
-        #     print("bioRxiv entry not found.")
-
-
-class MedRxivPreprintStrategy(PreprintStrategy):
-    def update_preprint(self, entry):
-        print(f"Checking medRxiv preprint: {entry['fields']['title']}")
-        return entry
-        # result = self._make_api_request(entry['fields']['doi'])
-        # if result:
-        #     print(f"medRxiv entry found: {result}")
-        # else:
-        #     print("medRxiv entry not found.")
-
-
-class arxivPreprintStrategy(PreprintStrategy):
-    def update_preprint(self, entry):
-        print(f"Checking arxiv preprint: {entry['fields']['title']}")
-        return entry
-
+        print(f"Checking a preprint: {entry['fields']['title']}")
+        if "doi" in entry["fields"]:
+            response = self._make_api_request(entry["fields"]["doi"])
+            if not self.published_key in response['collection'][0]:
+                print("Publication is not yet published")
+                return entry
+            # the preprint is now published, we need to update the entry
+            published_entry = self.handle_published(response)
+            return published_entry
+        else:
+            raise NotImplementedError("Will need to find DOI somehow....")
 
 # these are all supported preprint servers
 # add lowercase key and class here, class implementation above
+# insert links without last slash to make code above more readable
 preprint_strategies = {
-    "biorxiv": BioRxivPreprintStrategy(),
-    "medrxiv": MedRxivPreprintStrategy(),
-    "arxiv": arxivPreprintStrategy()
+    "biorxiv":
+        PreprintHandler(
+            "bioRxiv",
+            {"api_url": "https://api.biorxiv.org/pubs/biorxiv",
+             "published_key": "published_doi"
+             }),
+    "medrxiv":
+        PreprintHandler("arXiv",
+                        {"api_url":
+                             "https://api.biorxiv.org/pubs/medrxiv",
+
+                         }),
+    "arxiv":
+        PreprintHandler("", {
+            "api_url": "",
+            "published_key": ""
+            # todo this seesm to not exist..
+            #  in this case I will have to google scholar
+            # todo also need to set api_url for arxiv and figure out next steps
+        })
 }
